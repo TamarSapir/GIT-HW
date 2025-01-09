d11fbd4105726089c05ddf2180c13c4d74bbdfc6
Tamar <Tamarsapir05@gmail.com>
2025-01-09 14:49:40 +0200

    PK!71½{[Content_Types].xml ¢( ¬TKoÂ0¾OÚ¨rh`i(ö8nH°·h(6þýÜðÐ4K£&ñ÷°Çë¦NVÐ8AÚ	ØÜicËL|Ï>zÏ"ARV«ÚYÈÄPG÷wÃÙÆ&m1ó ©ó`ù¤p¡QĿ¡^åU|ì÷dî – 2025-01-09 14:49:40 –  –  – ¥µb4|B-kJÞ׼½U27V$¯Û{-U&÷µÉ±P¹²úIÏÉA»|Ù0t>ÒXPS§>fS bc(äQÎ

--- commit.sh
+++ commit.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then #check the paramters
-    echo "Usage: $0 <TASKID> <Dev Message> [--push] [<REPO>]"
+if [ "$#" -ne 1 ]; then #check paramters
+    echo "Usage: $0 <input_csv_file>"
     exit 1
 fi
 
 #according the tesk the code will get just two parametrs 
-TASKID=$1
-DEV_MSG=$2
-PUSH=false  
-REPO_PATH="." #this folder
+CSV_FILE=$1
 
-for arg in "$@"; do #check if we can push
-    if [ "$arg" == "--push" ]; then
-        PUSH=true
-    elif [[ ! "$arg" == "$1" && ! "$arg" == "$2" && ! "$arg" == "--push" ]]; then
-        REPO_PATH="$arg"
-    fi
-done
-
-#check if the folder wxist
-if [ ! -d "$REPO_PATH/.git" ]; then
-    echo "Error: $REPO_PATH is not a valid Git repository."
+#check if there are csv file
+if [ ! -f "$CSV_FILE" ]; then
+    echo "Error: File $CSV_FILE does not exist."
     exit 1
 fi
 
-cd "$REPO_PATH" || exit
+#reading the scv
+while IFS=',' read -r TASKID DESC BRANCH DEVELOPER GITHUB_URL; do
+    if [ "$TASKID" == "TaskID" ]; then
+        continue
+    fi
 
-CURRENT_DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
-BRANCH_NAME=$(git branch --show-current)
-DEV_NAME="TAMAR SAPIR" 
-TASK_DESC="Create README.md file" 
+    CURRENT_DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+    COMMIT_MSG="$TASKID – $CURRENT_DATETIME – $BRANCH – $DEVELOPER – $DESC"
 
-#commit
-COMMIT_MSG="$TASKID – $CURRENT_DATETIME – $BRANCH_NAME – $DEV_NAME – $TASK_DESC – $DEV_MSG"
+    #branch
+    git checkout "$BRANCH" || git checkout -b "$BRANCH"
 
-git add .
-git commit -m "$COMMIT_MSG"
+    #commit
+    git add .
+    git commit -m "$COMMIT_MSG"
+
+    #push
+    git push "$GITHUB_URL" "$BRANCH"
+    if [ $? -eq 0 ]; then
+        echo "Commit and push succeeded for TaskID $TASKID."
+    else
+        echo "Commit and push failed for TaskID $TASKID."
+    fi
+
+done < "$CSV_FILE"
