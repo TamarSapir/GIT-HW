e4f956058839fa3273eb3cdec830fdc899967e3c
Tamar <Tamarsapir05@gmail.com>
2025-01-09 14:38:17 +0200

    1000 – 2025-01-09 14:38:17 – master – TAMAR SAPIR – Create README.md file – Initial commit

--- commit.sh
+++ commit.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then #check the paramters
-    echo "Usage: $0 <TASKID> <Dev Message>"
+if [ "$#" -lt 2 ]; then #check the paramters
+    echo "Usage: $0 <TASKID> <Dev Message> [--push] [<REPO>]"
     exit 1
 fi
 
 #according the tesk the code will get just two parametrs 
 TASKID=$1
 DEV_MSG=$2
+PUSH=false  
+REPO_PATH="." #this folder
+
+for arg in "$@"; do #check if we can push
+    if [ "$arg" == "--push" ]; then
+        PUSH=true
+    elif [[ ! "$arg" == "$1" && ! "$arg" == "$2" && ! "$arg" == "--push" ]]; then
+        REPO_PATH="$arg"
+    fi
+done
+
+#check if the folder wxist
+if [ ! -d "$REPO_PATH/.git" ]; then
+    echo "Error: $REPO_PATH is not a valid Git repository."
+    exit 1
+fi
+
+cd "$REPO_PATH" || exit
 
 CURRENT_DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
 BRANCH_NAME=$(git branch --show-current)
